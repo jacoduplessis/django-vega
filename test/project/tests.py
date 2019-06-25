82d50f06255139ae8bd4507386f4083d165b14aa
Jaco du Plessis <jaco@jacoduplessis.co.za>
2019-06-25 08:51:57 +0200

    Update test

--- test/project/tests.py
+++ test/project/tests.py
@@ -3,22 +3,8 @@ from project.models import Sale
 import altair as alt
 from django_vega import render
 from project.utils import generate_sales
-import datetime
-import json
 import pathlib
 import base64
-
-
-class JSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime.datetime):
-            return obj.isoformat()
-        elif isinstance(obj, datetime.date):
-            return obj.isoformat()
-        elif isinstance(obj, datetime.timedelta):
-            return (datetime.datetime.min + obj).time().isoformat()
-        else:
-            return super().default(obj)
 
 
 class TestSimpleChart(TestCase):
@@ -39,9 +25,7 @@ class TestSimpleChart(TestCase):
             column='payment_method:N'
         )
 
-        output = render(chart.to_json(cls=JSONEncoder))
+        output = render(chart.to_dict())
 
-        pathlib.Path('test_2.svg').write_text(output['svg'])
-        pathlib.Path('test_2.png').write_bytes(base64.b64decode(output['png']))
-
-
+        pathlib.Path('test.svg').write_text(output['svg'])
+        pathlib.Path('test.png').write_bytes(base64.b64decode(output['png']))
